88b9e202ec3f310870e88af8c882f15a2cfa52f7
Shreshth <shreshthkanchan777@gmail.com>
2026-02-15 10:29:49 +0530

    /warn

---
+++ commands/moderation/warn.js
@@ -0,0 +1,43 @@
+import {
+  PermissionFlagsBits,
+  SlashCommandBuilder,
+} from "discord.js";
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName("warn")
+    .setDescription("Warn a member in the server.")
+    .addUserOption((option) =>
+      option
+        .setName("user")
+        .setDescription("User to be warned")
+        .setRequired(true),
+    )
+    .setDefaultMemberPermissions(PermissionFlagsBits.ModerateMembers)
+    .setDMPermission(false),
+    
+  async execute(interaction) {
+    // The specified user who is to be warned
+    const target = interaction.options.getUser("user", true);
+
+    await interaction.reply({
+      content: `Warning issued to ${target.tag}.`,
+      ephemeral: true,
+    });
+
+    if (interaction.channel) {
+      await interaction.channel.send(
+        `${target}, you've received a warning from ${interaction.user}.`,
+      );
+    }
+
+    try {
+      await target.send(
+        `You have been warned in ${interaction.guild?.name ?? "this server"} by ${interaction.user.tag}.`,
+      );
+    } catch (error) {
+      console.error("Failed to DM warned user:", error);
+    }
+  },
+};
+
